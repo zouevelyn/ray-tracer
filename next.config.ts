bd36b978fae89d8b5f98b7a9da65b2b4c33b3df7
Evelyn Zou <ez2170@rit.edu>
2025-01-23 15:10:29 -0500

    pushing out folder again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/ray-tracer",
+  output: "export",  // <=== enables static exports
 };
 
 export default nextConfig;
